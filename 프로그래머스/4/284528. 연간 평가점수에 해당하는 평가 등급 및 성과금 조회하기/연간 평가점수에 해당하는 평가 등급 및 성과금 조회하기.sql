e8b572306f11d65c02003161be056fc75a04c789
chaerin Yu <dbcofls6961@ajou.ac.kr>
2026-07-06 21:57:31 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,24 @@
+SELECT
+        HE.EMP_NO,
+        HE.EMP_NAME,
+        CASE
+            WHEN AVG(HG.SCORE) >= 96 THEN "S"
+            WHEN AVG(HG.SCORE) >= 90 THEN "A"
+            WHEN AVG(HG.SCORE) >= 80 THEN "B"
+            ELSE 'C'
+        END AS GRADE,
+        CASE
+            WHEN AVG(HG.SCORE) >= 96 THEN SAL * 0.2
+            WHEN AVG(HG.SCORE) >= 90 THEN SAL * 0.15
+            WHEN AVG(HG.SCORE) >= 80 THEN SAL * 0.1
+            ELSE 0
+        END AS BONUS
+FROM    HR_EMPLOYEES HE
+JOIN    HR_GRADE HG
+    ON  HE.EMP_NO = HG.EMP_NO
+GROUP BY
+        HE.EMP_NO,
+        HE.EMP_NAME,
+        HE.SAL
+ORDER BY
+        HE.EMP_NO
