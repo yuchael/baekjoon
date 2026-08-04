9e20166d90ab1875b6db64c7d5ea5dd8284f8a70
chaerin Yu <dbcofls6961@ajou.ac.kr>
2026-08-04 11:56:05 +0900

    [level 2] Title: 가장 큰 수, Time: 33.08 ms, Memory: 26.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,13 @@
+def solution(numbers):
+    answer = ''
+    
+    numbers = list(map(str, numbers))
+    
+    numbers.sort(key=lambda x: x * 3, reverse=True)
+    
+    answer = ''.join(numbers)
+    
+    if answer[0] == '0':
+        return '0'
+    
+    return answer
